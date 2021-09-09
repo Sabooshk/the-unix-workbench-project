2a95b596eb16ee59fc5d9a42188c539b65b583e2
Sabooshk <zach@razorsharppr.com>
2021-09-09 18:37:14 -0500

    modified guessinggame.sh and updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # The Unix Workbench Project
 Date: 
-Thu Sep  9 18:22:36 CDT 2021
+Thu Sep  9 18:36:07 CDT 2021
 
 Lines: 
 21 guessinggame.sh
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,21 +1,21 @@
-echo "How many files are in the current directory?"
-while :
-do
-	read -p 'Guess: ' response
-	if [[ $response -gt 3 ]]
-	then
-		echo "Fewer!"
-	elif [[ $response -lt 3 ]]
-	then
-		echo "Greater!"
-	else
-		echo "Correct!"
-		break
-	fi
-done
+function guessinggame {
+	echo "How many files are in the current directory?"
+	while :
+	do
+		read -p 'Guess: ' response
+		if [[ $response -gt 3 ]]
+		then
+			echo "Fewer!"
+		elif [[ $response -lt 3 ]]
+		then
+			echo "Greater!"
+		else
+			echo "Correct!"
+			break
+		fi
+	done
+}
 
-
-
-
+guessinggame
 
 #comment to make the line quota
